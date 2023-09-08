d62f5c2fed98ba7805fd762f14adfcae47f29ec1
Zena Lapp <zenalapp@gmail.com>
2023-09-08 09:52:55 -0600

    Add code and figure to make logo

---
+++ data-raw/make_logo.R
@@ -0,0 +1,17 @@
+(hexSticker::sticker("man/figures/mozzie_for_logo.png",
+  package = "bistro",
+  filename = "man/figures/logo.png",
+  s_x = 1,
+  s_y = 0.94,
+  s_width = 0.65,
+  p_x = 1.35,
+  p_y = 1.45,
+  p_color = "firebrick4",
+  p_size = 18,
+  h_fill = "ghostwhite",
+  h_size = 1.5,
+  h_color = "firebrick4"
+))
+
+
+
